112913d8c3f3dceb7aded0595ee2238ecfcf40dc
lokeshsinha1992 <80097031+lokeshsinha1992@users.noreply.github.com>
2022-06-13 18:15:30 +0530

    commit from intellij Commit 2 newasd

--- src/main/java/org/api/contants/Endpoints.java
+++ src/main/java/org/api/contants/Endpoints.java
@@ -9,6 +9,8 @@ public class Endpoints {
     public static final String CREATE_PERSON = "/persons";
     public static final String DELETE_PERSON = "/persons/{id}";
     public static final String UPDATE_PERSON = "/persons/{id}";
+
 // Comment 1
     // Comment 2
+
 }
